26179a68d6168dea691ca21de496d474c9a8e22d
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-06-12 18:53:09 -0700

    Add my layout on server.js and start an outline of what I want to use for my code

--- Develop/server.js
+++ Develop/server.js
@@ -1,1 +1,27 @@
 // Just send these files as they are and they don't need anything fancy to them
+
+// Sets up the Express App
+const app = express();
+const PORT = 3000;
+
+// Sets up the Express app to handle app parsing
+app.use(express.urlencoded);
+
+function handleRequest() {
+  //  case "/index.html":
+}
+
+// Basic route tht sends the user first to the AJAX Page // Change this for the file we want to send
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(_direname, "views", "add.html"));
+});
+
+app.get("/add", function (req, res) {
+  return `index.html`;
+});
+
+// Posting the actual notes and takes in JSON input
+app.post("/api/notes", function (req, res) {});
+
+// listen to this port here
+app.listen.PORT;
