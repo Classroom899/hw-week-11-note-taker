adedb6e85e40b346ce4bacd7b1a771937cdf4268
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-06-14 10:21:52 -0700

    Add the require and const noteData for where it needs to require the index.js information

--- Develop/routes/apiNotes.js
+++ Develop/routes/apiNotes.js
@@ -1,3 +1,7 @@
 // Routing
 require("./routes/apiNotes")(app);
-require("./routes/htmlRoutes")(app);
+// require("./routes/htmlRoutes")(app);
+
+const noteData = require("../public/assets/js/index");
+
+// Routing
