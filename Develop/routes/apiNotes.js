8f124ea092774807d0832bf47bbaa7d439807532
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-06-14 10:49:46 -0700

    Update the consts within apiNotes.js and what each requires along with add more information to the app.get requests

--- Develop/routes/apiNotes.js
+++ Develop/routes/apiNotes.js
@@ -2,12 +2,16 @@
 // require("./routes/apiNotes")(app);
 // require("./routes/htmlRoutes")(app);
 
-const noteData = require("../public/assets/js/index");
+const noteData = require("notes.html");
+const starData = require("index.html");
 
 // Routing
 module.exports = function (app) {
   app.get("/notes.html", function (req, res) {
     res.json(noteData);
+  });
+  app.get("/index.html", function (req, res) {
+    res.json(starData);
   });
 };
 
