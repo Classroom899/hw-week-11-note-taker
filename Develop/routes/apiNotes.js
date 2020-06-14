eec4d150a3e90a22365692e95d9b1a8cb99da5b7
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-06-14 14:41:14 -0700

    Create post request and add app.get requests for noteData and starData

--- Develop/routes/apiNotes.js
+++ Develop/routes/apiNotes.js
@@ -10,14 +10,22 @@ module.exports = function (app) {
   app.get("/notes.html", function (req, res) {
     res.json(noteData);
   });
+  app.get("/notes.html", function (req, res) {
+    res.sendFile(path.join(_dirname, "/routes/apiNotes.js"));
+  });
   app.get("/index.html", function (req, res) {
     res.json(starData);
   });
+  app.get("/index.html", function (req, res) {
+    res.sendFile(path.join(_dirname, "/routes/htmlRoutes.js"));
+  });
+
+  // POST request
   app.post("api/notes.html", function (req, res) {
     console.log(req.body);
   });
 };
 
-// app.get("/notes", function (req, res) {
-//     res.sendFile(path.join(_direname, "views", "notes.html"));
-//   });
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(_dirname, "/public/assets/js/index.js"));
+});
